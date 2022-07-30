344efd84ac3b2b9b9f80dc288717f9b2d670ec1a
Zac Ong <61817066+zzacong@users.noreply.github.com>
2022-07-30 12:48:47 +1000

    server session helper

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,66 +1,15 @@
+import type { GetServerSidePropsContext } from 'next'
+import { getServerSession } from '$server/helpers/get-server-session'
 import { trpc } from '$lib/trpc'
 
-type TechnologyCardProps = {
-  name: string
-  description: string
-  documentation: string
+export default function HomePage() {
+  return <div>Home</div>
 }
 
-export default function Home() {
-  const hello = trpc.useQuery(['example.hello', { text: 'from tRPC' }])
-
-  return (
-    <main className="container mx-auto flex h-screen flex-col items-center justify-center p-4">
-      <h1 className="text-5xl font-extrabold leading-normal text-gray-700 md:text-[5rem]">
-        Create <span className="text-purple-300">T3</span> App
-      </h1>
-      <p className="text-2xl text-gray-700">This stack uses:</p>
-      <div className="mt-3 grid gap-3 pt-3 text-center md:grid-cols-2 lg:w-2/3">
-        <TechnologyCard
-          name="NextJS"
-          description="The React framework for production"
-          documentation="https://nextjs.org/"
-        />
-        <TechnologyCard
-          name="TypeScript"
-          description="Strongly typed programming language that builds on JavaScript, giving you better tooling at any scale"
-          documentation="https://www.typescriptlang.org/"
-        />
-        <TechnologyCard
-          name="TailwindCSS"
-          description="Rapidly build modern websites without ever leaving your HTML"
-          documentation="https://tailwindcss.com/"
-        />
-        <TechnologyCard
-          name="tRPC"
-          description="End-to-end typesafe APIs made easy"
-          documentation="https://trpc.io/"
-        />
-      </div>
-      <div className="flex w-full items-center justify-center pt-6 text-2xl text-blue-500">
-        {hello.data ? <p>{hello.data.greeting}</p> : <p>Loading..</p>}
-      </div>
-    </main>
-  )
-}
-
-const TechnologyCard = ({
-  name,
-  description,
-  documentation,
-}: TechnologyCardProps) => {
-  return (
-    <section className="flex flex-col justify-center rounded border-2 border-gray-500 p-6 shadow-xl duration-500 motion-safe:hover:scale-105">
-      <h2 className="text-lg text-gray-700">{name}</h2>
-      <p className="text-sm text-gray-600">{description}</p>
-      <a
-        className="mt-3 text-sm text-violet-500 underline decoration-dotted underline-offset-2"
-        href={documentation}
-        target="_blank"
-        rel="noreferrer"
-      >
-        Documentation
-      </a>
-    </section>
-  )
+export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
+  return {
+    props: {
+      session: await getServerSession(ctx),
+    },
+  }
 }
